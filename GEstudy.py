6b328a3bc4533919e4c623440d2dde89e654d2fb
Fexxonn <torrenegradavid@gmail.com>
2024-07-03 18:57:13 -0500

    Second Commit

---
+++ GEstudy.py
@@ -0,0 +1,59 @@
+# Ejercicio 1: Imprimir "Hola, Mundo" si la condición es verdadera
+# Escribe un programa que imprima "Hola, Mundo" si la variable condicion es verdadera.
+
+# Ejercicio 2: Comparar dos números
+# Escribe un programa que compare dos números y muestre cuál es mayor.
+
+# Ejercicio 3: Número par o impar
+# Escribe un programa que verifique si un número dado es par o impar.
+
+# Ejercicio 4: Clasificación de edades
+# Escribe un programa que clasifique a una persona según su edad: niño (0-12), adolescente (13-17), adulto (18-64) o anciano (65+).
+
+# Ejercicio 5: Nota de examen
+# Escribe un programa que determine la calificación de un examen basado en la puntuación (A, B, C, D, F).
+
+# Ejercicio 6: Año bisiesto
+# Escribe un programa que determine si un año dado es bisiesto.
+
+# Ejercicio 7: Verificar un triángulo
+# Escribe un programa que determine si tres longitudes de lado forman un triángulo válido.
+
+# Ejercicio 8: Mayor de tres números
+# Escribe un programa que encuentre el mayor de tres números.
+
+# Ejercicio 9: Verificar vocal o consonante
+# Escribe un programa que verifique si una letra es una vocal o una consonante.
+
+# Ejercicio 10: Determinar el signo zodiacal
+# GEscribe un programa que determine el signo zodiacal basado en el día y mes de nacimiento.
+
+# Ejercicio 11: Conversión de grados Celsius a Fahrenheit
+# Escribe un programa que convierta una temperatura de grados Celsius a Fahrenheit.
+
+# Ejercicio 12: Calcular tarifa de envío
+# Escribe un programa que calcule la tarifa de envío basada en el peso del paquete.
+
+# Ejercicio 13: Categorizar temperatura
+# Escribe un programa que categorice la temperatura en "frío", "templado" o "caliente".
+
+# Ejercicio 14: Verificar si un número es múltiplo de otro
+# Escribe un programa que verifique si un número es múltiplo de otro.
+
+# Ejercicio 15: Descuento en tienda
+# Escribe un programa que aplique un descuento del 10% si la compra es mayor a 100.
+
+# Ejercicio 16: Calcular el índice de masa corporal (IMC)
+# Escribe un programa que calcule el IMC basado en la altura y el peso, e indique si la persona está bajo peso, en peso normal, sobrepeso o tiene obesidad.
+
+# Ejercicio 17: Identificar tipo de triángulo
+# Escribe un programa que determine el tipo de triángulo (equilátero, isósceles, escaleno) basado en las longitudes de sus lados.
+
+# Ejercicio 18: Determinar la fase de la vida
+# Escribe un programa que determine la fase de la vida de una persona basada en su edad (bebé, niño, adolescente, adulto, adulto mayor).
+
+# Ejercicio 19: Verificar si un número está dentro de un rango
+# Escribe un programa que verifique si un número está dentro de un rango (inclusive).
+
+# Ejercicio 20: Verificar si una cadena está vacía
+# Escribe un programa que verifique si una cadena está vacía.
