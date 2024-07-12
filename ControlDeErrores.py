cafe002265811d26b81b238ad7bef5879b3f0194
Fexxonn <torrenegradavid@gmail.com>
2024-07-11 20:21:50 -0500

    Second Commit

--- ControlDeErrores.py
+++ ControlDeErrores.py
@@ -4,15 +4,32 @@
 # else: // Solo se ejecuta cuando no hay una Excepciones
 # finally: // Siempre se ejecuta
 
-try:
-    num = int(input("Coloca un numero: ")) # 3
-    num2 = int(input("Coloca otro numero: ")) # 3.4
-    
-except:
-    print("no colocaste un numero correcto")
-    
-else: 
-    print(num + num2)
+##################################################################
 
- 
+# try:
+#     num = int(input("Coloca un numero: ")) # 3
+#     num_dos = int(input("Coloca otro numero: ")) # 3.4 
+# except:
+#     print("no colocaste un numero correcto")
+# else: 
+#     print(num + num_dos)
+
+#########################################################################
+
+# Calculadora: Pide dos valores enteros, multiplicalos cada uno por aparte y dividelos cada uno por aparte entre 0 
+
+# try:
     
+# num1 = int(input("Ingresa el primer valor: "))
+# num2 = int(input("Ingresa el segundo valor: "))
+# num3 = int(input("Ingresa el tercer valor: "))
+    
+# mult = num1 * num3
+# div = num1 / num3
+# mult2 = num2 * num3
+# div1 = num2 / num3
+    
+# except:
+#     print("Error")
+
+# Calculadora: Pide al usuario dos numeros y preguntale que operacion quiere realizar, si el usuario quiere dividir entre 0 evitar el error y mostrar un mensaje que diga "No se puede dividir entre 0"
