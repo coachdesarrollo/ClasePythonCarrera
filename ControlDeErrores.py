57c07e74415dea5e77226318e39ad1185065f9b5
Fexxonn <torrenegradavid@gmail.com>
2024-07-11 19:39:59 -0500

    Second Commit

--- ControlDeErrores.py
+++ ControlDeErrores.py
@@ -1,18 +1,18 @@
 # Excepciones // Sintaxis
 # try: // Ingresa todo el codigo que queremos poner en cuarentena
-# except:
-# else:
-# finally:
+# except: // agarra el error y ejecuta el codigo que nosotros coloquemos
+# else: // Solo se ejecuta cuando no hay una Excepciones
+# finally: // Siempre se ejecuta
 
 try:
     num = int(input("Coloca un numero: ")) # 3
     num2 = int(input("Coloca otro numero: ")) # 3.4
     
 except:
-    print("Coloca un numero correcto: ")
+    print("no colocaste un numero correcto")
+    
 else: 
     print(num + num2)
-finally:
-    print("Esta es la respuesta: ")
+
  
     
