36c0fe814f884efe7b4092c731a2dfd4131ed6cb
Fexxonn <torrenegradavid@gmail.com>
2024-07-15 21:08:39 -0500

    Second Commit

---
+++ Bucles.py
@@ -0,0 +1,34 @@
+# Bucle For
+
+# Sintaxis
+
+# for i in range(1, 11, 1):
+#     .....
+
+# Reto 1
+# Pedirle al usuario una variable y imprimirla 5 veces
+
+# text = str(input("Escribe algo: "))
+
+# for i in range(5):
+#     print(text)
+
+# Reto 2
+# Imprime 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+# for i in range(1, 11, 1):
+#     print(i)
+
+##########################################
+
+# Ejemplo Contador:
+for i in range(5, -1, -1):
+    for j in range(9, -1, -1):
+        for k in range(1000, -1, -1):
+            for g in range(1000, -1, -1):
+                print(f"Inicia en: {i}.{j}.{k}")
+
+
+
+
+
